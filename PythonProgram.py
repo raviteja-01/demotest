f0e161a85f8feb76c92b9dd1b9ee26a0bebdeef0
raviteja-01 <126378723+raviteja-01@users.noreply.github.com>
2023-03-27 12:01:19 +0530

    Updated the Palindrome Python program with function.

--- PythonProgram.py
+++ PythonProgram.py
@@ -1,6 +1,11 @@
+def isPalindrome(s):
+  rev_s = s[::-1]
+  if s== rev_s:
+    return True 
+  return False
+
 s = input() 
-rev_s = s[::-1]
-if s == rev_s:
+if isPalindrome(s):
   print(s,"is a Palindrome")
 else:
   print(s,"is not a Palindrome")
